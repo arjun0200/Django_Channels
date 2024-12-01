c0fe9a8468d07db342caa0b76f1e959d3c87b365
arjun0200 <arjunverma9717@gmail.com>
2024-12-01 21:15:49 +0530

    real time data transfer

--- app/consumers.py
+++ app/consumers.py
@@ -1,16 +1,25 @@
-# Topic - Routing
+# Topic - Real-time Data Example
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
+from time import sleep
+import asyncio
+
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         print('Websocket Connected...', event)
         self.send({
-        'type':'websocket.accept'
+        'type':'websocket.accept',
         })
-        
+    
     def websocket_receive(self, event):
-        print('Messaged Received...', event)
-        print('Messaged is ', event['text'])
+        print('Message received from Client', event)
+        print(event['text'])
+        for i in range(50):
+            self.send({
+                'type':'websocket.send',
+                'text': str(i)
+            })
+            sleep(1)
 
     def websocket_disconnect(self, event):
         print('Websocket Disconnected...', event)
@@ -20,12 +29,18 @@ class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print('Websocket Connected...', event)
         await self.send({
-        'type':'websocket.accept'
+        'type':'websocket.accept',
         })
-        
+    
     async def websocket_receive(self, event):
-        print('Messaged Received...', event)
-        print('Messaged is ', event['text'])
+        print('Message received from Client', event)
+        print(event['text'])
+        for i in range(50):
+            await self.send({
+                'type':'websocket.send',
+                'text': str(i)
+            })
+            await asyncio.sleep(1)
 
     async def websocket_disconnect(self, event):
         print('Websocket Disconnected...', event)
