fc53f38f75e82b44c9deba9013adf3910e59ee14
arjun0200 <arjunverma9717@gmail.com>
2024-12-01 18:57:29 +0530

    checked with postman url ws://127.0.0.1:8000/ws/sc/ rounting.py integerated

--- app/consumers.py
+++ app/consumers.py
@@ -1,40 +1,32 @@
-# Topic - Consumer
+# Topic - Routing
 from channels.consumer import SyncConsumer, AsyncConsumer
-
-"""
-Synchronous: SyncConsumer is synchronous, meaning it processes one request at a time, 
-blocking other requests until the current one is completed.
-"""
-
-
+from channels.exceptions import StopConsumer
 class MySyncConsumer(SyncConsumer):
-    # This handler is called when client initially opens a connection and is about to finish the WebSocket handshake.
     def websocket_connect(self, event):
-        print('Websocket Connected...')
-
-    # This handler is called when data received from Client
+        print('Websocket Connected...', event)
+        self.send({
+        'type':'websocket.accept'
+        })
+        
     def websocket_receive(self, event):
-        print('Messaged Received...')
+        print('Messaged Received...', event)
+        print('Messaged is ', event['text'])
 
-    # This handler is called when either connection to the client is lost, either from the client closing the connection, the server closing the connection, or loss of the socket.  
     def websocket_disconnect(self, event):
-        print('Websocket Disconnected...')
+        print('Websocket Disconnected...', event)
+        raise StopConsumer()
 
-"""
-Asynchronous: AsyncConsumer is asynchronous, meaning it can handle multiple connections concurrently without blocking. 
-It is capable of performing non-blocking I/O operations, which makes it ideal for scenarios that involve high concurrency or 
-long-running tasks like database queries, HTTP requests, etc.
-"""
-
-class MyAsyncConsumer(AsyncConsumer): ##handles multiple request
-    # This handler is called when client initially opens a connection and is about to finish the WebSocket handshake.
+class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
-        print('Websocket Connected...')
-
-    # This handler is called when data received from Client
+        print('Websocket Connected...', event)
+        await self.send({
+        'type':'websocket.accept'
+        })
+        
     async def websocket_receive(self, event):
-        print('Messaged Received...')
+        print('Messaged Received...', event)
+        print('Messaged is ', event['text'])
 
-    # This handler is called when either connection to the client is lost, either from the client closing the connection, the server closing the connection, or loss of the socket.  
     async def websocket_disconnect(self, event):
-        print('Websocket Disconnected...')
+        print('Websocket Disconnected...', event)
+        raise StopConsumer()
--- gs1/asgi.py
+++ gs1/asgi.py
@@ -1,19 +1,19 @@
-"""
-ASGI config for gs1 project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack  # Import the authentication middleware stack
+import app.routing  # Assuming 'app' is your app name containing routing.py
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs1.settings')
 
 application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(  # Wrap WebSocket routing with AuthMiddlewareStack
+        URLRouter(
+            app.routing.websocket_urlpatterns  # WebSocket URL routing
+        )
+    ),
 })
+
+
+###ws://127.0.0.1:8000/ws/sc/
