e352a2457471f43051c8baa93da5c311554a0031
arjun0200 <arjunverma9717@gmail.com>
2024-12-22 21:57:17 +0530

    authentication in chatapp

--- gs1/asgi.py
+++ gs1/asgi.py
@@ -1,19 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack  # Import the authentication middleware stack
-import app.routing  # Assuming 'app' is your app name containing routing.py
-
+import app.routing
+from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs1.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(  # Wrap WebSocket routing with AuthMiddlewareStack
-        URLRouter(
-            app.routing.websocket_urlpatterns  # WebSocket URL routing
-        )
-    ),
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(
+    URLRouter(
+      app.routing.websocket_urlpatterns
+    )
+  )
 })
-
-
-###ws://127.0.0.1:8000/ws/sc/
