a10d04c195d4257f544834e180beb367ebd7d4e4
arjun0200 <arjunverma9717@gmail.com>
2025-01-05 23:10:48 +0530

    live data to frontend websocket consumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,21 +1,24 @@
 # Topic - Generic Consumer - WebsocketConsumer and AsyncWebsocketConsumer
+# Real-time Data Example
+# Real-time Data Example with Front End
 
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from time import sleep
+import asyncio
 
 class MyWebsocketConsumer(WebsocketConsumer):
   # This handler is called when client initially opens a connection and is about to finish the WebSocket handshake.
   def connect(self):
     print('Websocket Connected...')
     self.accept()     # To accept the connection
-    # self.close()      # To reject the connection
   
   # This handler is called when data received from Client
   def receive(self, text_data=None, bytes_data=None):
     print('Message Received from Client...', text_data)
-    self.send(text_data="Message from server to Client")  # To send Data to Client
-    # self.send(bytes_data=data)    # To Send Binary Frame to Client
-    # self.close()                    # To force-close the connection
-    # self.close(code=4123)            # To Add a custom websocket error code
+    for i in range(20):
+      self.send(text_data=str(i))   # To send data to client
+      sleep(1)
+
 
   # This handler is called when either connection to the client is lost, either from the client closing the connection, the server closing the connection, or loss of the socket.
   def disconnect(self, close_code):
@@ -26,15 +29,13 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
   async def connect(self):
     print('Websocket Connected...')
     await self.accept()     # To accept the connection
-    # await self.close()      # To reject the connection
   
   # This handler is called when data received from Client
   async def receive(self, text_data=None, bytes_data=None):
     print('Message Received from Client...', text_data)
-    await self.send(text_data="Message from server to Client")  # To send Data to Client
-    # await self.send(bytes_data=data)    # To Send Binary Frame to Client
-    # await self.close()                    # To force-close the connection
-    # await self.close(code=4123)            # To Add a custom websocket error code
+    for i in range(20):
+      await self.send(text_data=str(i))   # To send data to client
+      await asyncio.sleep(1)
 
   # This handler is called when either connection to the client is lost, either from the client closing the connection, the server closing the connection, or loss of the socket.
   async def disconnect(self, close_code):
